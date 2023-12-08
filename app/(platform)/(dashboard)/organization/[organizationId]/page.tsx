dcd80bc9522e34b14f421de425a3ac6a3baa2667
liuxin <liuxin85@gmail.com>
2023-12-08 23:14:58 +0800

    build: organization page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,12 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  return (
+    <div>
+      {/* <OrganizationSwitcher /> */}
+      Organization Page
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
