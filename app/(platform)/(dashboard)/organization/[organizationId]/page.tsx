8dc07dd247fd5ccb8a2fcb2413e02137282981db
liuxin <liuxin85@gmail.com>
2023-12-09 23:14:46 +0800

    build: useAction hook

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,17 @@
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
   return (
-    <div>
-      {/* <OrganizationSwitcher /> */}
-      Organization Page
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
     </div>
   );
 };
